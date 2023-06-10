2d5ebe0f115f11710ba0cbabb35842471ce2b375
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-06-10 00:43:06 -0300

    desafios

---
+++ PHP/1-NúmerosÌmpares.php
@@ -0,0 +1,4 @@
+<?php
+    for($i = 0; $i < 100; $i++)
+        if($i % 2 != 0)
+            echo "#$i" . PHP_EOL;
---
+++ PHP/2-TabuadaDeMultiplicação.php
@@ -0,0 +1,7 @@
+<?php
+    for($i = 1; $i < 10; $i++){
+        for($g = 1; $g < 10; $g++){
+            echo $i . 'x' . $g . '=' . ($i * $g) . PHP_EOL;
+        }
+        echo PHP_EOL;
+    }
