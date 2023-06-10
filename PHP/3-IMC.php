238f1d36b58256ab04090160119c4a5b06394a76
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-06-10 01:32:09 -0300

    Algoritmo para cálculo do IMC

---
+++ PHP/3-IMC.php
@@ -0,0 +1,16 @@
+<?php
+    $peso = 100;
+    $altura = 1.60;
+
+    $imc = $peso / pow($altura, 2);
+    echo "IMC: $imc" . PHP_EOL;
+    if($imc < 18.5)
+        echo "CLASSIFICAÇÃO: MAGREZA";
+    elseif($imc >= 18.5 && $imc < 25)
+        echo "CLASSIFICAÇÃO: NORMAL";
+    elseif($imc >= 25 && $imc < 30)
+        echo "CLASSIFICAÇÃO: SOBREPESO";
+    elseif($imc >= 30 && $imc < 40)
+        echo "CLASSIFICAÇÃO: OBESIDADE";
+    elseif($imc >= 40)
+        echo "CLASSIFICAÇÃO: OBESIDADE GRAVE";
