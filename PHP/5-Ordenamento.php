45641851d973aa33ee496093f9456b45c17be2e4
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-25 23:54:46 -0300

    .

--- PHP/5-Ordenamento.php
+++ PHP/5-Ordenamento.php
@@ -10,7 +10,19 @@ $array = [
     ]
 ];
 
-var_dump($array);
+$notas1 = [
+    'Maria' => 10,
+    'Pedro' => 5,
+    'João' => 7,
+    'Isabel' => 8
+];
+
+$notas2 = [
+    10,
+    5,
+    7,
+    8
+];
 
 function ordenamento(array $array1, array $array2) :int
 {
@@ -26,4 +38,16 @@ function ordenamento(array $array1, array $array2) :int
 }
 
 usort(array: $array, callback: 'ordenamento');
-var_dump($array);
+// var_dump($array);
+
+asort($notas1); // Ordena as notas de forma crescente
+// arsort($notas1); // Ordena as notas de forma decrescente
+
+// ksort($notas1); // Ordena de forma crescente
+// krsort($notas1); // Ordena de forma decrescente
+
+sort($notas2); // Ordena de forma crescente
+// rsort($notas2); // Ordena de forma decrescente
+
+var_dump($notas1);
+var_dump($notas2);
