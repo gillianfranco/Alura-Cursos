a5a7d4317571affd8ace0bd7f0ad821bb84cbc93
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-25 23:37:37 -0300

    Ordenamento com critérios

--- PHP/5-Ordenamento.php
+++ PHP/5-Ordenamento.php
@@ -1,12 +1,29 @@
 <?php
 $array = [
-    1,
-    3,
-    4,
-    2
+    [
+        'aluno' => 'Gillian',
+        'nota' => 10
+    ],
+    [
+        'aluno' => 'Johnson',
+        'nota' => 8.5
+    ]
 ];
+
 var_dump($array);
 
-$arrayOrdenado = $array;
-sort($arrayOrdenado);
-var_dump($arrayOrdenado);
+function ordenamento(array $array1, array $array2) :int
+{
+    // if($array1['nota'] < $array2['nota']){
+    //     return -1;
+    // }
+    // if($array1['nota'] > $array2['nota']){
+    //     return 1;
+    // }
+    // return 0;
+
+    return $array1['nota'] <=> $array2['nota'];
+}
+
+usort(array: $array, callback: 'ordenamento');
+var_dump($array);
