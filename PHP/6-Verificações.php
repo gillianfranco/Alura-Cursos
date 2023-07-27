1d6b2ca0134c1fbe1b54109a7aca1208bebdb6e9
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-26 23:12:30 -0300

    Verificação de tipos de dados

---
+++ PHP/6-Verificações.php
@@ -0,0 +1,28 @@
+<?php
+    $inteiro = 1;
+    $float = 2.5;
+    $array = [1, 2, 3, 4, 5];
+    $lista = [
+        1 => 'Posição 1',
+        2 => 'Posição 2',
+        4 => 'Posição 3',
+        7 => 'Posição 4'
+    ];
+    $bool = true;
+    $nula;
+
+    if(is_int($inteiro)){
+        echo "{$inteiro} é um inteiro" . PHP_EOL;
+    }
+    if(is_float($float)){
+        echo "{$float} é um decimal" . PHP_EOL;
+    }
+    if(is_array($array)){
+        echo 'array é um array' . PHP_EOL;
+    }
+    if(array_is_list($lista)){
+        echo "{$lista} é uma lista" . PHP_EOL;
+    }
+    if(is_bool($bool)){
+        echo "{$bool} é booleano" . PHP_EOL;
+    }
