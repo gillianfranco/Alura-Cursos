ae9d1de5a6d2ac43e5cf48c99013052530abea0b
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-28 23:29:26 -0300

    Verificações nos arrays

--- PHP/7-VerificaçãoDeChave.php
+++ PHP/7-VerificaçãoDeChave.php
@@ -7,5 +7,11 @@
         'Ana' => null
     ];
 
+    echo 'A chave existe?' . PHP_EOL;
     var_dump(array_key_exists('Ana', $nota)); // Verifica apenas se a chave existe
-    var_dump(isset($nota['Ana'])); // Verifica se o valor da chave é válido
+    echo 'O valor desta chave é nulo?' . PHP_EOL;
+    var_dump(isset($nota['Ana'])); // Verifica se o valor da chave não é nulo
+    echo 'O valor existe dentro do array?' . PHP_EOL;
+    var_dump(in_array(10, $nota, false)); // Verifica se o valor existe dentro do array
+    echo 'Este valor está em alguma chave do array?' . PHP_EOL;
+    var_dump(array_search(10, $nota)); // Verifica qual chave tem o valor informado
