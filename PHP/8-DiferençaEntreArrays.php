d63ae56166b3f894f61d0a8d4b6be9eacbb30150
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-30 01:20:08 -0300

    Manipulação de Arrays

---
+++ PHP/8-DiferençaEntreArrays.php
@@ -0,0 +1,25 @@
+<?php
+    $primeiroBimestre = [
+        'Maria' => 10,
+        'João' => 7.5,
+        'Marta' => 8,
+        'Pedro' => 9
+    ];
+
+    $segundoBimestre = [
+        'Maria' => 9,
+        'João' => 9,
+        'Marta' => 10
+    ];
+
+    //var_dump(array_diff($primeiroBimestre, $segundoBimestre)); // Retorna a diferença de acordo com o valor dos índices
+    //var_dump(array_diff_key($primeiroBimestre, $segundoBimestre)); // Retorna a diferença de acordo com a índice
+    //var_dump(array_diff_assoc($primeiroBimestre, $segundoBimestre)); // Retorna a diferença de acordo com a associação entre o índice e o valor
+
+    $ausentesNoSegundoBimestre = array_diff_key($primeiroBimestre, $segundoBimestre);
+
+    $alunos = array_keys($ausentesNoSegundoBimestre); // Recupera os índices de um array
+    $notas = array_values($ausentesNoSegundoBimestre); // Recupera os valores de um array
+
+    var_dump(array_flip(array_combine($notas, $alunos))); // Combina e inverte os dados
+    
