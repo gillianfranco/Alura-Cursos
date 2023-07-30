7079ce1966e1afdcd5f142e8233fef895c3903a0
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-29 23:47:46 -0300

    array_diff

---
+++ PHP/8-DiferençasEntreArrays.php
@@ -0,0 +1,16 @@
+<?php
+    $primeiroBimestre = [
+        'Maria' => 10,
+        'João' => 7.5,
+        'Marta' => 8,
+        'Pedro' => 9
+    ];
+
+    $segundoBimestre = [
+        'Maria' => 9,
+        'João' => 9,
+        'Marta' => 10,
+        'Pedro' => 9
+    ];
+
+    //var_dump(array_diff($primeiroBimestre, $segundoBimestre)); // Retorna a diferença de acordo com o valor dos índices
