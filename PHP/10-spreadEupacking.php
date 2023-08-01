9e71471b4aea218e249fa436ba0359cd9281f53e
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-08-01 00:09:32 -0300

    Spread e Unpacking

--- PHP/10-spreadEupacking.php
+++ PHP/10-spreadEupacking.php
@@ -15,4 +15,21 @@
 
     $arrays = [...$array1, 'Gillian', ...$array2];
 
+    function spread(int ...$array){
+        return $array;
+    }
+
+    function unpacking(int $a, int $b, int $c){
+        $array = [$a, $b, $c];
+        return $array;
+    }
+
+    $unpacking = unpacking(...[1, 2, 3]);
+
+    $spread = spread(1, 2, 3, 4, 5, 6);
+
+    var_dump($spread);
+
+    var_dump($unpacking);
+
     var_dump($arrays);
