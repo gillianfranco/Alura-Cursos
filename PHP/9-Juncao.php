df269af7e7de820fe715b17e7d34a04438ffc818
Gillian Melo Franco <gillian.melo.franco17@gmail.com>
2023-07-31 23:41:57 -0300

    Junção de Arrays

---
+++ PHP/9-Juncao.php
@@ -0,0 +1,16 @@
+<?php
+    $array1 = [
+        'João',
+        'Pedro',
+        'Ana',
+        'Maria'
+    ];
+
+    $array2 = [
+        'José',
+        'Marta',
+        'Carlos',
+        'Isabel'
+    ];
+
+    var_dump(array_merge($array1, $array2));
