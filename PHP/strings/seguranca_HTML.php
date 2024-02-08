81c88674f83d641ce64f892c2c4a7c949abe75ed
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-07 22:27:37 -0300

    Adicionar 'seguranca.HTML'

---
+++ PHP/strings/seguranca_HTML.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = 'Gillian"';
+?>
+
+<input type="text" name="<?php echo addslashes($nome);  ?>">
+<input type="text" name="<?php echo $nome  ?>">
+
+<input type="text" name="<?php echo addslashes($nome);  ?>">
+<input type="text" name="<?php echo $nome  ?>">
