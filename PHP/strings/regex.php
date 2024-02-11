9e74801cdd2997949e680b7638c4bbfbbf8e094c
gillianfranco <gillian.melo.franco17@gmail.com>
2024-02-10 22:20:47 -0300

    Utilizar preg_replace();

--- PHP/strings/regex.php
+++ PHP/strings/regex.php
@@ -1,9 +1,11 @@
 <?php
     $phoneNumbers = ['(34) 9 97685204', '34 9 9775-4964'];
+    $regex = '@^\(?([0-9]{2})\)? ?(9? ?[89][5-9][0-9]{2}-?[0-9]{4})$@';
     
     foreach($phoneNumbers as $numbers){
-        $verifyNumber = preg_match("@^\(?[0-9]{2}\)? ?9? ?[89][5-9][0-9]{2}-?[0-9]{4}$@", $numbers, $matches);
+        $verifyNumber = preg_match($regex, $numbers, $matches);
         if($verifyNumber){
             echo "Número $numbers é válido!" . PHP_EOL;
+            echo preg_replace($regex, '(XX) \2', $numbers) . PHP_EOL;
         }
     }
